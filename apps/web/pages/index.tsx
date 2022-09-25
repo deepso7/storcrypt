229967f6dedc5c9d8f443d4692e4c014f3f613a7
Ramitphi <18ucs144@lnmiit.ac.in>
2022-09-25 17:48:24 +0530

    added: tabe route

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Image from "next/image";
+import Link from "next/link";
 import About from "../components/About";
 import Hero from "../components/Hero";
 import WalletLogin from "../components/WalletLogin";
@@ -12,6 +13,10 @@ const Home: NextPage = () => {
     <div className="h-full flex flex-col justify-start gap-20 px-20">
       {loggenIn ? <Hero /> : <WalletLogin />}
       <About />
+
+      <Link href="/table">
+        <a className="text-white">About Us</a>
+      </Link>
     </div>
   );
 };
